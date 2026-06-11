724eb7c422e4cc19c1a5e2dbd2536ca265931fa8
Ryan Palmer <ryanpalmercs@github.com>
2026-06-11 10:20:50 -0500

    Add initial schema flyway

---
+++ backend/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,84 @@
+CREATE TABLE rooms
+(
+    id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    room_number  INT           NOT NULL,
+    type         VARCHAR       NOT NULL,
+    nightly_rate DECIMAL(5, 2) NOT NULL,
+    status       VARCHAR       NOT NULL,
+    created_at   TIMESTAMPTZ   NOT NULL,
+    updated_at   TIMESTAMPTZ   NOT NULL
+);
+
+CREATE TABLE guests
+(
+    id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name   VARCHAR        NOT NULL,
+    last_name    VARCHAR        NOT NULL,
+    email        VARCHAR UNIQUE NOT NULL,
+    phone_number VARCHAR(10)    NOT NULL
+        CONSTRAINT check_only_digits
+            CHECK (phone_number ~ '^[0-9]+$'
+        AND LENGTH (phone_number) = 10,
+    created_at   TIMESTAMPTZ    NOT NULL,
+    updated_at   TIMESTAMPTZ    NOT NULL
+)
+    );
+
+CREATE TABLE reservations
+(
+    id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    guest_id       INT REFERENCES guests (id) NOT NULL,
+    room_id        INT REFERENCES rooms (id)  NOT NULL,
+    check_in_date  DATE                       NOT NULL,
+    check_out_date DATE                       NOT NULL,
+    status         VARCHAR                    NOT NULL,
+    checked_in_at  TIMESTAMPTZ,
+    checked_out_at TIMESTAMPTZ,
+    created_at     TIMESTAMPTZ                NOT NULL,
+    updated_at     TIMESTAMPTZ                NOT NULL
+);
+
+CREATE TABLE folios
+(
+    id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    reservation_id INT REFERENCES reservations (id) NOT NULL,
+    status         VARCHAR                          NOT NULL,
+    total          DECIMAL(8, 2)                    NOT NULL,
+    created_at     TIMESTAMPTZ                      NOT NULL,
+    updated_at     TIMESTAMPTZ                      NOT NULL
+);
+
+CREATE TABLE folio_items
+(
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    folio_id    INT REFERENCES folios (id) NOT NULL,
+    description VARCHAR                    NOT NULL,
+    amount      DECIMAL(8, 2)              NOT NULL,
+    type        VARCHAR                    NOT NULL,
+    created_at  TIMESTAMPTZ                NOT NULL,
+    updated_at  TIMESTAMPTZ                NOT NULL
+);
+
+CREATE TABLE employees
+(
+    id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR       NOT NULL,
+    last_name  VARCHAR       NOT NULL,
+    role       VARCHAR       NOT NULL,
+    pay_rate   DECIMAL(5, 2) NOT NULL,
+    hire_date  DATE          NOT NULL,
+    active     BOOLEAN       NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMPTZ   NOT NULL,
+    updated_at TIMESTAMPTZ   NOT NULL
+);
+
+CREATE TABLE time_entries
+(
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    employee_id INT REFERENCES employees (id) NOT NULL,
+    date        DATE                          NOT NULL,
+    hours       DECIMAL(4, 2)                 NOT NULL,
+    notes       VARCHAR,
+    created_at  TIMESTAMPTZ                   NOT NULL,
+    updated_at  TIMESTAMPTZ                   NOT NULL
+);
