1588fa499d226633ddffb232945a85b33993e072
Ryan Palmer <ryanpalmercs@github.com>
2026-06-11 13:46:03 -0500

    Update application.yml and build.gradle.kts to prep for jwt auth

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-oauth2-resource-server")
 
     runtimeOnly("org.postgresql:postgresql")
 
