6be6f29892e81401f3b875e8ae8eb973b8eca5aa
Ryan Palmer <ryanpalmercs@github.com>
2026-06-12 13:24:11 -0500

    Add room list page and modal for room inventory management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RoomsPage from './pages/RoomsPage'
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<div>Staydesk</div>} />
+                <Route path="/rooms" element={<RoomsPage />} />
             </Routes>
         </BrowserRouter>
     )
