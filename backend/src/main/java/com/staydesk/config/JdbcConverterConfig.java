931e2a5d7a3f03c3daf0bd2c1475c0b837bc6ee6
Ryan Palmer <ryanpalmercs@github.com>
2026-07-06 15:09:18 -0500

    Add EncryptedToken and TokenCipher and converted in JdbcConverterConfig (#65)

--- backend/src/main/java/com/staydesk/config/JdbcConverterConfig.java
+++ backend/src/main/java/com/staydesk/config/JdbcConverterConfig.java
@@ -1,8 +1,13 @@
 package com.staydesk.config;
 
+import ch.qos.logback.core.rolling.helper.TokenConverter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.staydesk.model.ContactInfo;
+import com.staydesk.model.EncryptedToken;
+import com.staydesk.security.TokenCipher;
 import org.postgresql.util.PGobject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -56,6 +61,38 @@ public class JdbcConverterConfig  {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    @WritingConverter
+    static class EncryptedTokenWriter implements Converter<EncryptedToken, String> {
+        private static final Logger LOGGER = LoggerFactory.getLogger(EncryptedTokenWriter.class);
+        private final TokenCipher tokenCipher;
+
+        EncryptedTokenWriter(TokenCipher tokenCipher) {
+            this.tokenCipher = tokenCipher;
+        }
+
+        @Override
+        public String convert(EncryptedToken source) {
+            LOGGER.info("Encrypting EncryptedToken for storage");
+            return tokenCipher.encrypt(source.value());
+        }
+    }
+
+    @ReadingConverter
+    static class EncryptedTokenReader implements Converter<String, EncryptedToken> {
+        private static final Logger LOGGER = LoggerFactory.getLogger(EncryptedTokenReader.class);
+        private final TokenCipher tokenCipher;
+
+        EncryptedTokenReader(TokenCipher tokenCipher) {
+            this.tokenCipher = tokenCipher;
+        }
+
+        @Override
+        public EncryptedToken convert(String source) {
+            LOGGER.info("Decrypting stored value into EncryptedToken");
+            return new EncryptedToken(tokenCipher.decrypt(source));
         }
     }
 }
