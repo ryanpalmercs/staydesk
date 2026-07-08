bef17eb23838f620fa8558395637df8d4420f4e5
Ryan Palmer <ryanpalmercs@github.com>
2026-07-08 08:00:52 -0500

    Add flagging to Guest and add new GuestNotFoundException (#98)

--- backend/src/main/java/com/staydesk/model/Guest.java
+++ backend/src/main/java/com/staydesk/model/Guest.java
@@ -3,12 +3,16 @@ package com.staydesk.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.lang.Nullable;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Table("guests")
-public record Guest(@Id int id, String firstName, String lastName, String email, String phoneNumber, LocalDateTime createdAt, LocalDateTime updatedAt) {
-
+public record Guest(@Id int id, String firstName, String lastName, String email, String phoneNumber,
+                    boolean flagged, @Nullable String flagReason, @Nullable LocalDateTime flaggedDate,
+                    @Nullable UUID flaggedBy, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    
     @JsonProperty()
     public String name() {
         return firstName + " " + lastName;
